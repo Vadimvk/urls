72336e35c99f654b2d379e412c56640415e350c3
Edge <777vadimvk@gmail.com>
2018-05-30 04:27:11 +0300

    implemented creating short_urls

--- app/home/home.js
+++ app/home/home.js
@@ -8,8 +8,8 @@ angular.module('urls.home', ['ui.router'])
             templateUrl: '/home/home.html',
             controller: 'HomeInCtrl',
             resolve: {
-                auth: function($auth, $state) {
-                    return $auth.validateUser().catch(function(){
+                auth: function ($auth, $state) {
+                    return $auth.validateUser().catch(function () {
                         // redirect unauthorized users to the login page
                         alert('Please, authorize');
                         $state.go('sign_in');
@@ -19,18 +19,39 @@ angular.module('urls.home', ['ui.router'])
         })
     }])
 
-    .controller('HomeInCtrl', function ($scope, $auth) {
-        $scope.handleSignOutBtnClick = function (){
-            $auth.signOut()
-                .then(function(resp) {
-                    alert('out');
-                    $state.go('sign_in');
-                    // handle success response
-                })
-                .catch(function(resp) {
-                    // handle error response
+    .controller('HomeInCtrl', function ($scope, $auth, $http) {
+            $scope.handleSignOutBtnClick = function () {
+                $auth.signOut()
+                    .then(function (resp) {
+                        alert('out');
+                        $state.go('sign_in');
+                        // handle success response
+                    })
+                    .catch(function (resp) {
+                        // handle error response
+                    });
+            };
+
+            $scope.sendUrls = function () {
+                var data = {
+                    url: $scope.urlForm.url,
+                    short_url: $scope.urlForm.shortUrl
+                };
+                $http({
+                    url: 'http://localhost:3000/urls/create.json',
+                    method: 'POST',
+                    dataType: 'json',
+                    data: JSON.stringify(data),
+                    success: function (res) {
+                        console.log(res);
+                    },
+                    error: function (err) {
+                        console.log(err);
+                    }
                 });
-        };
 
 
-    });
+            };
+
+        }
+    );
